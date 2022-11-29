b6a140ea2ed8bc9c2bd51e5cd5a041d2f3ef5c6e
Dario Turin <dturin1997@gmail.com>
2022-11-29 12:08:33 -0500

    Create student

---
+++ Database2022/Database2022/Services/EstudianteService.cs
@@ -0,0 +1,31 @@
+﻿using Database2022.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database2022.Services
+{
+    public class EstudianteService
+    {
+        private readonly AppDbContext _context;
+
+        public EstudianteService() => _context = App.GetContext();
+
+
+        public bool Create(Estudiante item)
+        {
+            try
+            {
+                //EntityFrameworkCore
+                _context.Estudiantes.Add(item);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
