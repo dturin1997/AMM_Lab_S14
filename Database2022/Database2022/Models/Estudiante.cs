c310083008daf175c02d82d44e54d02b1417a86a
Dario Turin <dturin1997@gmail.com>
2022-11-29 11:16:36 -0500

    Se avanzo el modelo

---
+++ Database2022/Database2022/Models/Estudiante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database2022.Models
+{
+    public class Estudiante
+    {
+        public int EstudianteID { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public double Nota { get; set; }
+        public bool Aprobado { get; set; }
+    }
+}
