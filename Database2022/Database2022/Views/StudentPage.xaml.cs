df58c58285eddc7fb71693f53f4e98cd340cd39c
Noe <noe@gmail.com>
2022-12-01 09:42:34 -0500

    Se agregaron las vistas y archivos de viewModel

---
+++ Database2022/Database2022/Views/StudentPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Database2022.Models;
+using Database2022.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Database2022.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StudentPage : ContentPage
+    {
+        public StudentPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new StudentViewModel(student);
+        }
+    }
+}
